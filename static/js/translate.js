6df434cef607cc4ceb558f3cb68b04b3c1688521
Spyro1 <redstone.mc01@gmail.com>
2025-07-20 18:04:35 +0200

    translation json files and script added

---
+++ static/js/translate.js
@@ -0,0 +1,34 @@
+const elements = document.querySelectorAll("[data-translate]");
+// const languageSelector = document.getElementById("language");
+const dropdown = document.getElementById("language-selector");
+const label = dropdown.querySelector(".dropdown__label");
+const options = dropdown.querySelectorAll(".dropdown__option");
+
+function setLanguage(lang) {
+  fetch(`translations/${lang}.json`)
+    .then(res => res.json())
+    .then(translations => {
+      elements.forEach(el => {
+        const key = el.getAttribute("data-translate");
+        if (translations[key]) {
+          el.textContent = translations[key];
+        }
+      });
+      document.documentElement.lang = lang;
+      localStorage.setItem("language", lang);   // Save selected language
+    //   label.textContent = lang;                 // Update dropdown label
+    });
+}
+
+// Handle custom dropdown
+options.forEach(option => {
+  option.addEventListener("click", () => {
+    const selectedLang = option.textContent.trim();
+    label.textContent = selectedLang;       // Update visible label
+    setLanguage(selectedLang);              // Apply translation
+  });
+});
+
+// Load saved language or default to English
+const savedLang = localStorage.getItem("language") || "en";
+setLanguage(savedLang);
